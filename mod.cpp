cfaf121ea9b866ce795b0117ed9acda4cb8159fd
honorlglory <honorlglory@naver.com>
2017-02-17 18:21:46 +0900

    로고 및 모드파일 추가
    로고와 모드파일 추가함

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Kimchiguys Foward Outpost Base Construction system on ACE3MOD";
+picture = "KG_logo.paa";
+logo = "KG_logo.paa";
+logoOver = "KG_logo.paa";
+actionName = "Github";
+action = "https://github.com/honorlglory/KG_FOB_ACE";
+description = "KGFOB";
+tooltip = "KGFOB";
+tooltipOwned = "Kimchiguys Owned";
+overview = "KG FOB system is wonderful!!";
+overviewPicture = "KG_logo.paa";
+author = "Kimchiguys Kukipa";
